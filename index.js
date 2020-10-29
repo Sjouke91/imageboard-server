1233c2c0f9a475794eaabf1de8d671de2ad353f0
Sjouke91 <s.bosma1991@gmail.com>
2020-10-29 09:05:50 +0100

    adding post route to image

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routers/user");
 const imageRouter = require("./routers/image");
+const jsonParser = express.json();
 PORT = 4000;
+
+app.use(jsonParser);
 
 app.use("/user", userRouter);
 app.use("/image", imageRouter);
--- routers/image.js
+++ routers/image.js
@@ -15,4 +15,17 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const { title, url } = req.body;
+    if (!title || !url) {
+      res.status(400).send("this request needs a title and an url");
+    }
+    const newImage = await Image.create({ title, url });
+    res.send(newImage);
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
