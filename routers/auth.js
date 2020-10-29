ca964aeb0d904bab7721d18f7bb2f07229854891
Sjouke91 <s.bosma1991@gmail.com>
2020-10-29 12:35:09 +0100

    added password verification

--- routers/auth.js
+++ routers/auth.js
@@ -1,21 +1,43 @@
 const { Router } = require("express");
 const { toJWT, toData } = require("../auth/jwt");
+const User = require("../models").user;
+const bcrypt = require("bcrypt");
 
 const router = new Router();
 
-router.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password) {
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).send({
+      message: "Please supply a valid email and password",
+    });
+  } else {
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
       res.status(400).send({
-        message: "Please supply a valid email and password",
+        message: "User with that email does not exist",
+      });
+    }
+    // 2. use bcrypt.compareSync to check the password against the stored hash
+    else if (bcrypt.compareSync(password, user.password)) {
+      // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+      const jwt = toJWT({ userId: user.id });
+      res.send({
+        jwt,
+      });
+    } else {
+      res.status(400).send({
+        message: "Password was incorrect",
       });
     }
     res.send({
       jwt: toJWT({ userId: 1 }),
     });
-  } catch (e) {
-    next(e);
   }
 });
 
