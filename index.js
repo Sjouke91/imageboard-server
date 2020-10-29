2e153620c745d946ba6fcacb1a979082142adeed
Sjouke91 <s.bosma1991@gmail.com>
2020-10-29 12:15:19 +0100

    Added login functionality

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routers/user");
 const imageRouter = require("./routers/image");
+const auth = require("./routers/auth");
 const jsonParser = express.json();
 PORT = 4000;
 
 app.use(jsonParser);
+app.use(auth);
 
 app.use("/user", userRouter);
-app.use("/image", imageRouter);
+app.use("/image/auth/messy", imageRouter);
 
 app.listen(PORT, () => console.log("listening on port:", PORT));
--- routers/image.js
+++ routers/image.js
@@ -1,17 +1,45 @@
 const { Router } = require("express");
 const router = new Router();
 const Image = require("../models").image;
+const { toJWT, toData } = require("../auth/jwt");
+
+// router.get("/", async (req, res, next) => {
+//   try {
+//     const images = await Image.findAll();
+//     if (!images) {
+//       res.status(404).send("no images found");
+//     } else {
+//       res.send(images);
+//     }
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// router.get("/", (req, res, next) => {
+//   const limit = Math.min(req.query.limit || 25, 500);
+//   const offset = req.query.offset || 0;
+
+//   Image.findAndCountAll({ limit, offset })
+//     .then((result) => res.send({ images: result.rows, total: result.count }))
+//     .catch((error) => next(error));
+// });
 
 router.get("/", async (req, res, next) => {
-  try {
-    const images = await Image.findAll();
-    if (!images) {
-      res.status(404).send("no images found");
-    } else {
-      res.send(images);
+  const auth =
+    req.headers.authorization && req.headers.authorization.split(" ");
+  if (auth && auth[0] === "Bearer" && auth[1]) {
+    try {
+      const data = toData(auth[1]);
+    } catch (e) {
+      res.status(400).send("Invalid JWT token");
     }
-  } catch (e) {
-    next(e);
+    const allImages = await Image.findAll();
+    res.json(allImages);
+  } else {
+    res.status(401).send({
+      message: "Please supply some valid credentials",
+    });
   }
 });
 
